cf6111d7f06848be76cc87d61396dd8202629962
Spacetime <justinecantado@gmail.com>
2017-09-04 22:08:13 +0800

    Update comments_controller.rb

--- app/controllers/spree/admin/comments_controller.rb
+++ app/controllers/spree/admin/comments_controller.rb
@@ -2,6 +2,6 @@ class Spree::Admin::CommentsController < Spree::Admin::ResourceController
   private
   
   def location_after_save
-    redirect_to root_url
+    redirect_to (:back), :notice => "create comment" and return
   end
 end
