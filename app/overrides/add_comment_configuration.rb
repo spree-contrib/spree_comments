2f4f2a9716597e08aa07d2fba3df08a741ee17d4
johndavid400 <johndavid400@gmail.com>
2019-09-16 21:30:15 -0500

    only show admin comment menu item if admin

--- app/overrides/add_comment_configuration.rb
+++ app/overrides/add_comment_configuration.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_configuration",
                      :name => "converted_admin_configurations_menu_468573024",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => "<%= configurations_sidebar_menu_item Spree.t(:comment_types), admin_comment_types_path %>",
+                     :text => "<%- if spree_current_user.admin? %><%= configurations_sidebar_menu_item Spree.t(:comment_types), admin_comment_types_path %><% end %>",
                      :disabled => false)
